36306d48cd0c3253e6c2d28ee2c5c561adad3d6c
nileshpawar0212 <nileshpawar0212@gmail.com>
2024-03-02 12:27:10 +0530

    var1

---
+++ variables.tf
@@ -0,0 +1,16 @@
+
+variable "filename" {
+    default = "/root/pets.txt"
+}
+variable "content" {
+    default = "We love pets!"
+}
+variable "prefix" {
+      default = "Mrs"
+}
+variable "separator" {
+    dedefault = "."    
+}
+variable "length" {
+    default = "1"
+}
