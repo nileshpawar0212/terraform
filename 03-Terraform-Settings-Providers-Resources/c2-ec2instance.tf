ec2fa0fa5cf5e0d034e45c4253db26b25268768b
nileshpawar0212 <nileshpawar0212@gmail.com>
2024-03-15 16:40:56 +0530

    edit

--- 03-Terraform-Settings-Providers-Resources/c2-ec2instance.tf
+++ 03-Terraform-Settings-Providers-Resources/c2-ec2instance.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "myec2vm" {
   ami = "ami-0d7a109bf30624c99"
   instance_type = "t3.micro"
-  user_data = file("$(path.module)/app1-install.sh")
+  user_data = file("${path.module}/app1-install.sh")
   tags = {
     "Name" = "EC2 Demo"
   }
