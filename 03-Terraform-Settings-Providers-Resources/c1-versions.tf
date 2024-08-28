0db7d01b17a51cb7f0c0677476d5a0de66a0737e
nileshpawar0212 <nileshpawar0212@gmail.com>
2024-08-28 21:22:51 +0530

    c1-1

--- 03-Terraform-Settings-Providers-Resources/c1-versions.tf
+++ 03-Terraform-Settings-Providers-Resources/c1-versions.tf
@@ -1,1 +1,13 @@
-2
+terraform {
+  required_version = "~> 1.19"
+  required_providers {
+    aws = {
+        source = "harshicorp/aws"
+        version = "~>5.63"
+    }
+  }
+}
+
+provider "aws" {
+    region = "us-east-2"
+    }
